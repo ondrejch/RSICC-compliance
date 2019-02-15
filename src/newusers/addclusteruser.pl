1586801d675fe4d837f480edca3d08cbe455ba23
Ondrej Chvala <ondrejch@gmail.com>
2019-02-15 14:55:33 -0500

    upates

--- src/newusers/addclusteruser.pl
+++ src/newusers/addclusteruser.pl
@@ -8,19 +8,23 @@ use warnings; use strict;
 # User input
 print "New user netid: ";
 my $netid = <STDIN>; chomp $netid;
-print "New user name/desc: ";
-my $uname = <STDIN>; chomp $uname;
 
 # Check if user exists
 my $dogrep = system("grep ^$netid /etc/passwd > /dev/null");
-print "User: $netid, $uname \n";
 if (not $dogrep) { die "User  $netid exists, quitting"; }
 
+# User description
+print "New user name/desc: ";
+my $uname = <STDIN>; chomp $uname;
+
 # Make user
+print "Making user: $netid, $uname \n";
 my $pwg = `pwgen 15 1`; chomp $pwg;
 system("adduser -c \"$uname\" $netid");
 system("echo $netid:$pwg | chpasswd");
-print "Your necluster login is your NetID, pass: $pwg\nPlease check https://necluster.ne.utk.edu/wiki/\n";
+system("make -C /var/yp");
+
+print "\nYour necluster login is your NetID, pass: $pwg\nPlease check https://necluster.ne.utk.edu/wiki/\n\n";
 
 # Setup user
 system("runuser -u $netid ./setup_env.sh");
--- src/newusers/setup_env.sh
+++ src/newusers/setup_env.sh
@@ -4,5 +4,5 @@
 cat /dev/zero | ssh-keygen -q -N '' > /dev/null
 ssh-keyscan necluster.ne.utk.edu >> ~/.ssh/known_hosts > /dev/null
 cat ~/.ssh/id_rsa.pub >>  ~/.ssh/authorized_keys
-ssh necluster.ne.utk.edu hostname
+ssh -i ~/.ssh/id_rsa necluster.ne.utk.edu hostname
 
